d693605625cb8f2aced76c74de4007821b80fada
zpschang <zps112358@126.com>
2017-09-27 20:05:50 +0800

    delete

--- feature.py
+++
@@ -1,8 +0,0 @@
-from analyze import load_catagory
-
-class feature_extractor():
-    def __index__(self):
-        self.catagory, self.word_to_catagory = load_catagory()
-
-    def extract(self, batch):
-        pass
